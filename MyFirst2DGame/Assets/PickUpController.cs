f41d41457ccd1aa20bcc6185ee1af967e91b6392
ChenCheng-Ho <chenaho@gmail.com>
2016-10-01 23:08:57 +0800

    Add PickUp Object Script to make it rotate

--- MyFirst2DGame/Assets/PickUpController.cs
+++ MyFirst2DGame/Assets/PickUpController.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class PickUpController : MonoBehaviour {
 
+
+	public float RotateSpeed=45f;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,6 @@ public class PickUpController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.transform.Rotate (new Vector3(0,0,RotateSpeed*Time.deltaTime));
 	}
 }
