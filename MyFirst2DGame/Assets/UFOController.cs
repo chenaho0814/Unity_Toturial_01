50a0ce64d166af73dff8475c2cfafabef0901450
ChenCheng-Ho <chenaho@gmail.com>
2016-10-01 17:00:04 +0800

    Add debug log information

--- MyFirst2DGame/Assets/UFOController.cs
+++ MyFirst2DGame/Assets/UFOController.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class UFOController : MonoBehaviour {
 
+	int i=0; 
+
 	// Use this for initialization
 	void Start () {
+		Debug.Log("Hello Start()");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log("Hello Update" + i );
+		i=i+1;
 	
 	}
 }
