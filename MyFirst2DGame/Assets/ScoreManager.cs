4cae8ab8ab2e38c5bc56de8f3c1dfba4b1fdfaf5
chenaho0814 <chenaho@cs.nccu.edu.tw>
2016-10-14 02:47:17 +0800

    Add Win / Lose Rule

--- MyFirst2DGame/Assets/ScoreManager.cs
+++ MyFirst2DGame/Assets/ScoreManager.cs
@@ -8,24 +8,53 @@ public class ScoreManager : MonoBehaviour {
 	private float _currentScore = 0;
 	const string ScorePrefix = "Score : ";
 
+    public int Score_Win=100;
+    public int Score_Lose= -10;
 
-	// Use this for initialization
-	void Start () {
+    private bool m_bIsGameEnd = false;
+
+    // Use this for initialization
+    void Start () {
 		_text = this.GetComponent<Text> ();
 		_text.text = ScorePrefix + _currentScore;
 	
+        
+
 	}
 
 
 	public void AddScore(int score)
 	{
+        if (m_bIsGameEnd == true) return;
+
 		_currentScore += score;
 		_text.text = ScorePrefix + _currentScore;
 	}
 
-	
+    void CheckGameStatus() {
+
+        if (_currentScore >= Score_Win) {
+            _text.text = "Win!!!";
+            _text.color = Color.green;
+            m_bIsGameEnd = true;
+        }
+
+
+        if (_currentScore <= Score_Lose)
+        {
+            _text.text =  "Lose...";
+            _text.color = Color.red;
+            m_bIsGameEnd = true;
+        }
+
+    }
+
+
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if(m_bIsGameEnd == false)
+            CheckGameStatus();
+
+    }
 }
