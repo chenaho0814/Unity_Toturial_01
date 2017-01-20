8ef1161de5b143a8e2c720629a7de05d2f2b9438
ChenCheng-Ho <chenaho@gmail.com>
2017-01-21 02:28:52 +0800

    Homework 2 - 控制pickUp的生成範圍

--- MyFirst2DGame/Assets/PickUpGenerator.cs
+++ MyFirst2DGame/Assets/PickUpGenerator.cs
@@ -20,6 +20,28 @@ public class PickUpGenerator : MonoBehaviour {
             int nRandPositionX = Random.Range(-10, 10) ;
             int nRandPositionY = Random.Range(-10, 10);
 
+			///Home work-2
+			// solution-1
+			if( nRandPositionX <3  && nRandPositionX >=0) nRandPositionX =  3 + Random.Range(0,7) ;
+			if( nRandPositionX >-3 && nRandPositionX <=0) nRandPositionX = -3 + Random.Range(0 ,-7);
+
+			if( nRandPositionY <3  && nRandPositionY >=0) nRandPositionY = 3 + Random.Range(0,7) ;
+			if( nRandPositionY >-3 && nRandPositionY <=0) nRandPositionY = -3  + Random.Range(0 ,-7);
+
+			// solution-2
+			//			while(nRandPositionX <= 3 && nRandPositionY <= 3)
+			//			{
+			//					nRandPositionX = Random.Range(-10, 10);
+			//				    nRandPositionY = Random.Range(-10, 10);
+			//			}    
+
+			// solution-2 version3
+			//			if( Mathf.Abs( nRandPositionX ) <=3 ) nRandPositionX =  3 + Random.Range(0,7) ;
+			//			if( Mathf.Abs( nRandPositionY ) <=3 ) nRandPositionY =  3 + Random.Range(0,7) ;
+
+
+
+
 
             PickUpController obj = GameObject.Instantiate(pickUpPrefab);
 
