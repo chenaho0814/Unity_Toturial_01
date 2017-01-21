c0a1441711183e63d08f38ac2526ed67f212dbe3
ChenCheng-Ho <chenaho@gmail.com>
2017-01-21 15:46:55 +0800

    CH10-Animator and Scripts to change status

---
+++ MyFirst2DGame/Assets/UFOAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFOAnimation : MonoBehaviour {
+
+	GameObject UFO;
+
+	// Use this for initialization
+	void Start () {
+		UFO = GameObject.Find("UFO");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown( KeyCode.T)  ){
+			UFO.gameObject.GetComponent<Animator>().SetTrigger("IsHit");
+		}
+	
+	}
+}
