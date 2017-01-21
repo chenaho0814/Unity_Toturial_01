f1bff5139e869f4d50cac4f36bf2354de9d8f8a8
ChenCheng-Ho <chenaho@gmail.com>
2017-01-21 20:02:57 +0800

    CH10 - DoTween Camera Shake

---
+++ MyFirst2DGame/Assets/CH10_DoTweenCameraShake.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class CH10_DoTweenCameraShake : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.T )){
+			Camera.main.transform.DOShakePosition(0.5f , 1   );	
+		}
+	}
+}
